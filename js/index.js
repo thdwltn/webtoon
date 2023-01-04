fe254b51dac34ef98125590bae7d7f07731305f8
thdwltn <godthdwltn1996@gmail.com>
2023-01-04 20:31:45 +0900

    css 작성완료

--- js/index.js
+++ js/index.js
@@ -1,6 +1,43 @@
-function side(){
-  $('aside .side>.front').on('click',function(e){
-      $(this).animate({marginRight:100+'px'},400)
-      return false;
+// install
+// import
+import $ from 'jquery'
+
+$(function(){
+
+
+  // $(mouse).onmouseover = function(){
+  //   over();
+  // }
+over();
+close();
+open();
+})
+
+function over(){
+  $('.main>ul>li').on('mouseenter',function(e){
+    $(this).children('div.see').css({display:'block'}).fadeIn()
+  })
+  $('.main>ul>li').on('mouseleave',function(e){
+    $(this).children('div.see').css({display:'none'}).fadeOut()
   })
 }
+
+function close(){
+  $('.close').on('click',function(e){
+    $('.fix').css('display','none')
+  })
+}
+
+function open(){
+  $('.event>p').on('click',function(e){
+    $('.main').css({display:'block'})
+  })
+}
+
+
+//   
+//   
+//  $mouse.on('mouseenter',function(e){
+// 
+//     $see.css({display:'block',transform:'scale(1.1)'});
+//   })
